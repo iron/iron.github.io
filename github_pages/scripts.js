1e9d46ce027adf995e1c5e77be414f2d6e4cfc6b
theptrk <patrick.tran06@gmail.com>
2014-06-27 23:43:14 -0700

    (feat) Added jquery

---
+++ github_pages/scripts.js
@@ -0,0 +1,7 @@
+$('.dropdown-toggle').on('mouseenter', function(){
+    $('.doc-note').fadeIn('slow');
+});
+
+$('.dropdown-toggle').on('mouseout', function(){
+    $('.doc-note').fadeOut('slow');
+});
